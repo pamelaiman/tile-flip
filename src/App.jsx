f5edc75e59548d3bed42a51a1695b1d77c82c804
Pamela Iman <117228797+pamelaiman@users.noreply.github.com>
2024-06-26 14:08:22 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { DemonstratingProps } from "./components/demo/DemonstratingProps";
+
+function App() {
+    return (
+        <div>
+            <h1>App component</h1>
+            <DemonstratingProps />
+        </div>
+    );
+}
+
+export default App;
